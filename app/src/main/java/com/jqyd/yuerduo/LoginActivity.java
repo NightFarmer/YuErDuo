a2ee1e29c94c0e714a19233f32a4bcee445a72e5
NightFarmer <494090597@qq.com>
2015-12-20 23:12:32 +0800

    登陆临时提交

--- app/src/main/java/com/jqyd/yuerduo/LoginActivity.java
+++ app/src/main/java/com/jqyd/yuerduo/LoginActivity.java
@@ -1,17 +1,35 @@
 package com.jqyd.yuerduo;
 
+import android.opengl.ETC1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.et_username)
+    EditText et_username;
+
+    @Bind(R.id.et_password)
+    EditText et_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
     }
 
 
+    @OnClick(R.id.bt_login)
+    public void onSubmit(){
+
+    }
 }
